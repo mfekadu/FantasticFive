f831b81bfcfcb080a2f0a461c7199f4e10145e6f
jlfisher <jfishe13@calpoly.edu>
2019-02-13 13:40:38 -0800

    Added AdminHeader and Inventory

--- client/src/router.ts
+++ client/src/router.ts
@@ -6,6 +6,8 @@ import Checkout from "./views/Checkout.vue";
 import Confirmation from "./views/Confirmation.vue";
 import Track from "./views/Track.vue";
 import Status from "./views/Status.vue";
+import Login from "./views/Login.vue";
+import Inventory from "./views/Inventory.vue";
 
 Vue.use(Router);
 
@@ -42,6 +44,16 @@ export default new Router({
       path: "/status",
       name: "status",
       component: Status
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/inventory",
+      name: "inventory",
+      component: Inventory
     }
   ]
 });
