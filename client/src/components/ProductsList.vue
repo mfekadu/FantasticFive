ceacd704d04f0ea1180b1893647bf1ad529bf15b
mfekadu <mike.fekadu@gmail.com>
2019-02-27 23:02:53 -0800

    update ProductsList to have data binding / list rendering
    https://vuejs.org/v2/guide/list.html

--- client/src/components/ProductsList.vue
+++ client/src/components/ProductsList.vue
@@ -19,31 +19,13 @@
 
 <template>
     <div class="products">
-        <div class="product">
-            <figure class="image is-128x128">
-                <img src="https://bulma.io/images/placeholders/128x128.png">
-            </figure>
-            <div class="name">Bike</div>
-            <div class="description">Bike Description</div>
-            <div class="price">$FREE.99</div>
-            <button>Add to Cart</button>
-        </div>
-        <div class="product">
+        <div class="product" v-for="(p, index) in products" v-bind:key="index">
             <figure class="image is-128x128">
                 <img  src="https://cdn.shopify.com/s/files/1/0799/9645/products/16_-_Right_900x.jpg?v=1547158720" alt="[BIKE_NAME]">
             </figure>
-            <div class="name">Bike</div>
-            <div class="description">Bike Description</div>
-            <div class="price">$FREE.99</div>
-            <button>Add to Cart</button>
-        </div>
-        <div class="product">
-            <figure class="image is-128x128">
-                <img  src="https://cdn.shopify.com/s/files/1/0799/9645/products/16_-_Right_900x.jpg?v=1547158720" alt="[BIKE_NAME]">
-            </figure>
-            <div class="name">Bike</div>
-            <div class="description">Bike Description</div>
-            <div class="price">$FREE.99</div>
+            <div class="title">{{ " " + p.title }}</div>
+            <div class="description">{{ " " + p.desc }}</div>
+            <div class="price">{{ " $FREE." + p.price }}</div>
             <button>Add to Cart</button>
         </div>
     </div>
@@ -53,8 +35,34 @@
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 
+interface Product {
+  title: string;
+  desc: string;
+  quantity: number;
+  price: number;
+  photoURL: string;
+}
+
 @Component
-export default class ProductsList extends Vue {}
+export default class ProductsList extends Vue {
+  product1: Product = {
+    title: "Bike",
+    desc: "description",
+    quantity: 5,
+    price: 99,
+    photoURL: "foo"
+  };
+
+  products: Product[] = [
+    this.product1,
+    this.product1,
+    this.product1,
+    this.product1,
+    this.product1,
+    this.product1,
+    this.product1
+  ];
+}
 </script>
 
 <style scoped>
