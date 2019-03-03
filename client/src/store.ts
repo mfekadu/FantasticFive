02ab706e604315b06f4247f4bd22b37d58a6317f
mfekadu <mike.fekadu@gmail.com>
2019-03-02 20:05:53 -0800

    refactor addToCart for readabilty

--- client/src/store.ts
+++ client/src/store.ts
@@ -40,21 +40,22 @@ const mutations: MutationTree<iRootState> = {
     state.userToken = null;
     state.user = null;
   },
-  // given an iRootState and an iProduct, mutate the cart. O(1) runtime
+  // given an iRootState and an iProduct, mutate the cart by adding the product. O(1) runtime
   // NOTE: id 0 is not possible in mysql so expect payload.id never 0
   // test case: (3 Trek Bikes in stock) add 2 Trek Bikes, click Cart, notice 2 Trek Bikes in cart, 
   // ... click Shop, add 1 Trek Bike, add 1 more, notice alert, click Cart, notice 3 Trek Bikes in cart
   addToCart(state: iRootState, product: iProduct) {
-    // if the product is in the cart
-    if (state.cart[product.id]) {
-      if (state.cart[product.id].inventoryQuantity <= state.cart[product.id].cartQuantity) {
-        // if out of stock
-        console.log("out of stock",state.cart[product.id].inventoryQuantity,"<=",state.cart[product.id].cartQuantity);
-        alert("out of stock");
-      } else { 
-        // else have stock, so grab another off the shelves
-        state.cart[product.id].cartQuantity++;
-      }
+    const productInCart: boolean = state.cart[product.id] ? true : false;
+    const inventoryQuantity: number = productInCart ? state.cart[product.id].inventoryQuantity : 0; 
+    const cartQuantity: number = productInCart ? state.cart[product.id].cartQuantity : 0;
+    const productIsOutOfStock: boolean = productInCart && (inventoryQuantity <= cartQuantity);
+
+    if (productInCart && productIsOutOfStock) {
+      console.log("out of stock",state.cart[product.id].inventoryQuantity,"<=",state.cart[product.id].cartQuantity);
+      alert("out of stock");
+    } else if (productInCart) {
+      // have stock, so grab another off the shelves
+      state.cart[product.id].cartQuantity++;
     } else {
       // otherwise add the product to the cart
       state.cart[product.id] = product;
