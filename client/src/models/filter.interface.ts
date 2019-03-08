597220be08c4211950b7d8b7884ac38393dff370
mfekadu <mike.fekadu@gmail.com>
2019-03-08 14:55:05 -0800

    npm run lint --fix

--- client/src/models/filter.interface.ts
+++ client/src/models/filter.interface.ts
@@ -1,14 +1,14 @@
 export interface iFilter {
-  type: string;    // brand, category, price, picked
-  value: string;   // a value corresponding to a key in an iProduct object
+  type: string; // brand, category, price, picked
+  value: string; // a value corresponding to a key in an iProduct object
   status: boolean; // to include or not include in filtered array
-};
+}
 
 export interface iAllFilters {
-  brands : iFilter[], 
-  categories : iFilter[],
-  prices : iFilter[],
-  shipping : iFilter
+  brands: iFilter[];
+  categories: iFilter[];
+  prices: iFilter[];
+  shipping: iFilter;
 }
 
 export enum PRICES {
@@ -20,22 +20,22 @@ export enum PRICES {
 
 // filter type enum
 export enum FT {
-  b = 'brand',
-  c = 'category',
-  p = 'price',
-  s = 'shipping'
+  b = "brand",
+  c = "category",
+  p = "price",
+  s = "shipping"
 }
 
 // will display just canShipYN = true
 export const CAN_SHIP: iFilter = {
-  type   : FT.s,
-  value  : FT.s,
-  status : true
+  type: FT.s,
+  value: FT.s,
+  status: true
 };
 
 // will display just canShipYN = false
 export const DEFAULT_SHIP: iFilter = {
-  type   : FT.s,
-  value  : FT.s,
-  status : false
+  type: FT.s,
+  value: FT.s,
+  status: false
 };
--- client/src/utils/array.utils.ts
+++ client/src/utils/array.utils.ts
@@ -2,7 +2,7 @@
 // allows for 'augmenting' the global scope in this file
 // do this ... `import "@/utils/array.utils"`;
 // Global Array is affected
-export {}
+export {};
 
 // Augment the global scope with extra functions for Array type
 // must be declared in a global scope because `Array` is global environment
@@ -22,20 +22,22 @@ declare global {
 Array.prototype.union = function(arr: any[]) {
   // loop through once for each value in this array.
   // make each object into a string so Set can compare
-  const strEachThis = this.map((value) => JSON.stringify(value))
+  const strEachThis = this.map(value => JSON.stringify(value));
   // same for given array
-  const strEachArr = arr.map((value) => JSON.stringify(value))
+  const strEachArr = arr.map(value => JSON.stringify(value));
   // Set is new in ES6 and it is an array with 'unique' values (no duplicates).
   const setThis = new Set(strEachThis);
   const setArr = new Set(strEachArr);
-  // "destructure" each Set into an Array and concatenate them. 
-  const concatenated = [...setThis, ...setArr]
+  // "destructure" each Set into an Array and concatenate them.
+  const concatenated = [...setThis, ...setArr];
   const strOnion = new Set(concatenated);
   let onion: any[] = [];
-  strOnion.forEach((val) => {onion.push(JSON.parse(val))});
+  strOnion.forEach(val => {
+    onion.push(JSON.parse(val));
+  });
   // dont cry, here's the pre-chopped onion:
   return onion;
-}
+};
 // define an alias
 Array.prototype.or = Array.prototype.union;
 
@@ -45,19 +47,21 @@ Array.prototype.or = Array.prototype.union;
 Array.prototype.intersection = function(arr: any[]) {
   // loop through once for each value in this array.
   // make each object into a string so Set can compare
-  const strEachThis = this.map((value) => JSON.stringify(value))
+  const strEachThis = this.map(value => JSON.stringify(value));
   // same for given array
-  const strEachArr = arr.map((value) => JSON.stringify(value))
+  const strEachArr = arr.map(value => JSON.stringify(value));
   // Set is new in ES6 and it is an array with 'unique' values (no duplicates).
   const setThis = new Set(strEachThis);
   const setArr = new Set(strEachArr);
   let strIntersection = [...setThis].filter(x => setArr.has(x));
   // console.log("filteredByIntersect...",filteredByIntersect);
   let intersection: any[] = [];
-  strIntersection.forEach((val) => {intersection.push(JSON.parse(val))});
+  strIntersection.forEach(val => {
+    intersection.push(JSON.parse(val));
+  });
   // let intersection = new Set(filteredByIntersect);
   // console.log("Array.from(intersection)...",Array.from(intersection),intersection);
-  return intersection;// Array.from(intersection);
-}
+  return intersection; // Array.from(intersection);
+};
 // define an alias
 Array.prototype.and = Array.prototype.intersection;
