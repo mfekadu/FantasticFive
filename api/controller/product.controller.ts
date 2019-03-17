de331a35a3ed18ea195d8a9785a36ac88ef40b87
mfekadu <mike.fekadu@gmail.com>
2019-03-17 12:43:48 -0700

    update 'updateProduct' and 'createProduct'

--- api/controller/product.controller.ts
+++ api/controller/product.controller.ts
@@ -2,9 +2,9 @@ import DefaultController from "./default.controller";
 
 import { Request, Response, Router } from "express";
 
-import { Product } from "../entity";
+import { Product, ProductBrand, ProductCategory } from "../entity";
 
-import { getRepository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 
 const OK = 200;
 
@@ -24,22 +24,42 @@ export class ProductController extends DefaultController {
              };
             // find the product table in DB, 
             // then set OK status and send back the productArray
-            productRepo.find().then(getProductArray);
+            const options = {relations: ['brand', 'categories']};
+            productRepo.find(options).then(getProductArray);
         };
 
         // private helper function to handle POST requests
         const createProduct = (req: Request, res: Response) => {
             const productRepo = getRepository(Product);
+            // const brandRepo = getRepository(ProductBrand);
+            // const categoryRepo = getRepository(ProductCategory);
             // unravel the req.body properties into variables
-            const { title, desc, stock, price, photoURL } = req.body;
-            const product = new Product();
-            product.title = title;
-            product.desc = desc;
-            product.stock = stock;
-            product.price = price;
-            product.photoURL = photoURL;
+            const { title, desc, stock, brand, categories,
+                    price, saleYN, salesPrice, canShipYN, photoURL } = req.body;
+            const newProduct = new Product();
+            newProduct.title = title;
+            newProduct.desc = desc;
+            const newBrand = new ProductBrand();
+            newBrand.name = brand.name;
+            // saveToRepo(brandRepo, newBrand);
+            console.log(newBrand);
+            newProduct.brand = newBrand;
+            let listofCats: ProductCategory[] = [];
+            for (const key in categories) {
+                const cat = categories[key];
+                const newCat = new ProductCategory();
+                newCat.name = cat.name
+                listofCats.push(newCat);
+            }
+            newProduct.categories = listofCats;
+            newProduct.stock = stock;
+            newProduct.price = price;
+            newProduct.saleYN = saleYN;
+            newProduct.salesPrice = salesPrice;
+            newProduct.canShipYN = canShipYN;
+            newProduct.photoURL = photoURL;
             // save the product, set OK, send back product
-            productRepo.save(product).then((createdProduct : Product) => {
+            productRepo.save(newProduct).then((createdProduct : Product) => {
                 res.status(OK).send({createdProduct});
             });
         };
@@ -48,14 +68,20 @@ export class ProductController extends DefaultController {
         const updateProduct = (req: Request, res: Response) => {
             const productRepo = getRepository(Product);
             // unravel the req.body properties into variables
-            const { title, desc, stock, price, photoURL } = req.body;
+            const { title, desc, brand, categories, stock, price, saleYN, 
+                    salesPrice, canShipYN, photoURL } = req.body;
             // get the product to be updated
             productRepo.findOneOrFail(req.params.id).then((foundProduct: Product) => {
                 // update the actual product
                 foundProduct.title = title;
                 foundProduct.desc = desc;
+                // foundProduct.brand.name = brand.name;
+                foundProduct.categories = categories;
                 foundProduct.stock = stock;
                 foundProduct.price = price;
+                foundProduct.saleYN = saleYN;
+                foundProduct.salesPrice = salesPrice;
+                foundProduct.canShipYN = canShipYN;
                 foundProduct.photoURL = photoURL;
                 // save the updated product
                 productRepo.save(foundProduct).then((updatedProduct : Product) => {
@@ -80,6 +106,7 @@ export class ProductController extends DefaultController {
             .post( createProduct );
 
         router.route("/shop/:id")
+        // add a get here
             .put( updateProduct )
             .delete( deleteProduct );
 
